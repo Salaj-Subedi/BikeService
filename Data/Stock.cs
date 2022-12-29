145b2a42729291b11ad09a5c31d44b5086bc770d
Salaj Subedi <SalajSubedi11@gmail.com>
2022-12-29 21:20:23 +0545

    Add project files.

---
+++ Data/Stock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeService.Data
+{
+    public class Stock
+    {
+        private List<StockItem> _items;
+
+        public Stock()
+        {
+            _items = new List<StockItem>();
+        }
+
+        public void AddItem(StockItem item)
+        {
+            _items.Add(item);
+        }
+
+        public void RemoveItem(StockItem item)
+        {
+            _items.Remove(item);
+        }
+
+        public List<StockItem> GetItems()
+        {
+            return _items;
+        }
+
+    }
+}
---
+++ Data/TimeSlot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeService.Data
+{
+    public class TimeSlot
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public TimeSlot(DateTime startTime, DateTime endTime)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public bool IsWithinTimeSlot(DateTime currentTime)
+        {
+            // Check if the current time is within the specified time slot
+            if (currentTime.TimeOfDay >= StartTime.TimeOfDay && currentTime.TimeOfDay <= EndTime.TimeOfDay)
+            {
+                // Check if the current day is a weekday (Monday to Friday)
+                if (currentTime.DayOfWeek >= DayOfWeek.Monday && currentTime.DayOfWeek <= DayOfWeek.Friday)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+}
